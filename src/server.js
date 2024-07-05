7a6f4334ede581f426c5d69178f59021664572a1
angad114782 <128778662+angad114782@users.noreply.github.com>
2024-07-05 21:08:40 +0530

    Commit message describing your changes

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const app = require(".");
 const { connectDb } = require("./config/db");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ;
 app.listen(PORT, async () => {
     await connectDb();
     console.log("API listening on port", PORT);
